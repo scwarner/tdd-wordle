1a305f9873f24d5242cbcaa855f221fbc6ce9c26
Sarah Hayes <hayes.sc@gmail.com>
2024-03-24 10:38:37 -0400

    Set up file to store constants

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,17 +1,13 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import {VICTORY_MESSAGE} from '../../settings'
 
 describe('WordleBoard', () => {
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    // Arrange 
     const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" }})
-
-    // Act
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("TESTS")
     await guessInput.trigger("keydown.enter")
-
-    //Application
-    expect(wrapper.text()).toContain("You won!")
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 })
