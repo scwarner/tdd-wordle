466b07d2a65ac7f0e2f9ebf8ab9f02440ab92b7e
Sarah Hayes <hayes.sc@gmail.com>
2024-03-24 11:20:09 -0400

    Move wrapper into beforeEach

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -4,8 +4,11 @@ import {VICTORY_MESSAGE, ERROR_MESSAGE} from '../../settings'
 
 describe('WordleBoard', () => {
   let wordOfTheDay = "TESTS"
+  let wrapper: ReturnType<typeof mount>;
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay }})
+  })
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay }})
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue(wordOfTheDay)
     await guessInput.trigger("keydown.enter")
@@ -13,14 +16,12 @@ describe('WordleBoard', () => {
   })
 
   test("a defeat message appears if user makes incorrect guess", async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay }})
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("PIZZA")
     await guessInput.trigger("keydown.enter")
     expect(wrapper.text()).toContain(ERROR_MESSAGE)
   })
   test("no end-of-game message appears if user has not yet made a guess", async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay }})
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
     expect(wrapper.text()).not.toContain(ERROR_MESSAGE)
 
